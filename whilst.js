7360ad5df0dd0a195623737301085a2c1b13594b
Farr <sfarri@byui.edu>
2018-11-05 16:40:31 -0700

    commit

--- whilst.js
+++ whilst.js
@@ -1,25 +1,6 @@
-var async = require('async'),
-    http = require('http'),
-    response = "",
-    count = 0,
-    url = process.argv[2];
+var http = require('http'),
+    async = require('async'),
+        url = process.argv[2],
+        test = "";
 
-async.whilst(() => {
-        return response !== 'meerkat';
-    }, callback => {
-        http.get(url, res => {
-            count++;
-            var body = "";
-            res.on('data', chunk => {
-                body += chunk.toString();
-            }).on('end', () => {
-                response = body.trim();
-                callback();
-            });
-        }).on('error', err => {
-            return console.log(err);
-        });
-    }, err => {
-        console.log(count);
-    }
-);
+async.whilst(() => { return test != "meerkat"}, (callback) => {  })
